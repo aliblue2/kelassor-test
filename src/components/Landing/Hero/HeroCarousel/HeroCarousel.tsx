bdd5e0a3e346a3f6ad19f4197ee6a6b9edb45027
Mohmd <mim.mohrechi@gmail.com>
2024-06-02 00:51:12 +0330

    fix hero carousel design

--- src/components/Landing/Hero/HeroCarousel/HeroCarousel.tsx
+++ src/components/Landing/Hero/HeroCarousel/HeroCarousel.tsx
@@ -38,10 +38,8 @@ const HeroCarousel = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-[190px] md:h-[450px]">
-      {/*todo <div className="relative min-w-[343px] h-[193px] md:min-w-[745px] md:h-[450px]"> */}
-      {/* carousel with cutout ********************************************************************************/}
-      {/*todo <div className="flex overflow-hidden heroCarouselSmall size-full md:heroCarouselBig"> */}
+    <div className="flex relative justify-center w-full h-[190px] md:h-[450px]">
+      {/* carousel content ********************************************************************************/}
       <div className="flex overflow-hidden size-full rounded-[20px] md:rounded-[50px]">
         {data.map((item) => (
           <div
@@ -51,6 +49,7 @@ const HeroCarousel = () => {
             className="relative duration-500 size-full shrink-0"
             key={item.title}
           >
+            {/* image ********************************************************************************/}
             <Image
               className="object-cover absolute size-full brightness-50"
               width={800}
@@ -58,7 +57,8 @@ const HeroCarousel = () => {
               src={item.image}
               alt="coding"
             />
-            <div className="flex absolute gap-5 flex-col justify-center items-center text-xl font-extrabold text-white size-full">
+            {/* text ********************************************************************************/}
+            <div className="flex absolute flex-col gap-5 justify-center items-center text-xl font-extrabold text-white size-full">
               <h1>بوت‌کمپ</h1>
               <h2>{item.title}</h2>
               <h2>{item.title2}</h2>
@@ -67,32 +67,39 @@ const HeroCarousel = () => {
         ))}
       </div>
 
-      {/* buttons and text ********************************************************************************/}
-      <div className="flex absolute top-0 right-0 bottom-0 left-0 flex-col justify-end items-center md:-bottom-2">
-        <div className="flex bg-background rounded-t-[20px] md:rounded-t-[50px] md:px-5 md:pt-5 px-2 pt-2 gap-4 md:gap-6">
-          <button
-            className="flex justify-center items-center bg-white rounded-full size-6 shadow2 md:size-[60px] hover:[&>*]:scale-125"
-            onClick={prevIndex}
-          >
-            <ChevronLeftIcon className="relative duration-200 size-3 right-[1px] text-gray-2 md:size-8" />
-          </button>
-          <div className="flex gap-2 justify-center items-center">
-            {data.map((item, i) => (
-              <span
-                key={item.title}
-                className={`rounded-full size-2 md:size-3 shadow2 duration-500 ${
-                  index === i ? "bg-primary-base" : "bg-light-2"
-                }`}
-              />
-            ))}
-          </div>
-          <button
-            className="flex justify-center items-center bg-white rounded-full size-6 shadow2 md:size-[60px] hover:[&>*]:scale-125"
-            onClick={nextIndex}
-          >
-            <ChevronRightIcon className="relative duration-200 left-[1px] size-3 text-gray-2 md:size-8" />
-          </button>
+      {/* buttons ********************************************************************************/}
+      <div className="flex absolute bottom-0 gap-4 px-2 pt-2 md:gap-6 md:px-5 md:pt-2 bg-background rounded-t-[15px] h-[30px] md:h-[60px] md:rounded-t-[30px]">
+        {/* left and right fancy corners ********************************************************************************/}
+        <div className="absolute bottom-0 rounded-br-full w-full h-1/2 right-full md:shadow-[50px_0_0_0_theme(colors.background)] shadow-[30px_0_0_0_theme(colors.background)]" />
+        <div className="absolute bottom-0 rounded-bl-full w-full h-1/2 left-full md:shadow-[-50px_0_0_0_theme(colors.background)] shadow-[-30px_0_0_0_theme(colors.background)]" />
+
+        {/* prev button ********************************************************************************/}
+        <button
+          className="flex justify-center items-center bg-white relative rounded-full size-6 shadow2 md:size-[60px] hover:[&>*]:scale-125"
+          onClick={prevIndex}
+        >
+          <ChevronLeftIcon className="relative duration-200 size-3 right-[1px] text-gray-2 md:size-8" />
+        </button>
+
+        {/* indicator ********************************************************************************/}
+        <div className="flex gap-2 justify-center items-center">
+          {data.map((item, i) => (
+            <span
+              key={item.title}
+              className={`rounded-full size-2 md:size-3 shadow2 duration-500 ${
+                index === i ? "bg-primary-base" : "bg-light-2"
+              }`}
+            />
+          ))}
         </div>
+
+        {/* next button ********************************************************************************/}
+        <button
+          className="flex justify-center items-center bg-white rounded-full relative size-6 shadow2 md:size-[60px] hover:[&>*]:scale-125"
+          onClick={nextIndex}
+        >
+          <ChevronRightIcon className="relative duration-200 left-[1px] size-3 text-gray-2 md:size-8" />
+        </button>
       </div>
     </div>
   );
