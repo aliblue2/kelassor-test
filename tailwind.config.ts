5cd773676bfcbff709cdee50b4994f4eac3ee617
Mohmd <mim.mohrechi@gmail.com>
2024-06-01 20:20:53 +0330

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,47 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: { vazir: ["Vazir", "sans"] },
+      colors: {
+        primary: {
+          base: "#145B51",
+          20: "#145B51",
+          30: "#115047",
+          40: "#0F443D",
+          50: "#0C3933",
+        },
+        secondary: {
+          base: "#46BFB4",
+          20: "#389990",
+          30: "#5E9C93",
+          50: "#8CB8B2",
+          75: "#C5DCD9",
+        },
+        black: "#0C0C0C",
+        background: "#FBFBFB",
+        gray: {
+          1: "#2D2D2D",
+          2: "#444444",
+          3: "#505050",
+          4: "#717171",
+        },
+        light: {
+          1: "#777778",
+          2: "#C7C6C6",
+          3: "#e7e7e7",
+          4: "#f5f5f5",
+        },
+        success: "#198754",
+        "success-light": "#D1F7E5",
+        error: "#C91433",
+        "error-light": "#FAE7EB",
+      },
+      boxShadow: {
+        "custom-lg":
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+        shadow1: "-2px 2px 10px 0px rgba(140,144,149,.12)",
+        shadow2: "-20px 20px 15px 0px rgba(140,144,149,1)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
