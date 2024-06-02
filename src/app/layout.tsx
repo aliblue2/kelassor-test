19a05ef8f0497d86ab67079ead4de3858814eb47
Mohmd <mim.mohrechi@gmail.com>
2024-06-02 20:23:15 +0330

    minor fix: maze overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ const RootLayout = async ({ children }: RootLayoutProps) => {
   ];
   return (
     <html lang="en">
-      <body className="px-5 mx-auto w-full max-w-[1200px] bg-background font-vazir">
+      <body className="px-5 mx-auto w-screen overflow-x-hidden max-w-[1200px] bg-background font-vazir">
         <NavBar bootcamps={bootcamps} />
         {children}
       </body>
