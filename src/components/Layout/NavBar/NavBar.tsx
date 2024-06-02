7ae1ab054a3e74c68089b069e792729398102f79
Mohmd <mim.mohrechi@gmail.com>
2024-06-02 13:26:56 +0330

    add animation to NavBar

--- src/components/Layout/NavBar/NavBar.tsx
+++ src/components/Layout/NavBar/NavBar.tsx
@@ -2,14 +2,19 @@
 import Nav from "./Nav/Nav";
 import CustomButton from "@/components/Ui/CustomButton";
 import { bootcamp } from "@/types/bootcamp";
+import { motion } from "framer-motion";
 import { MenuIcon } from "lucide-react";
-import Image from "next/image";
+//import Image from "next/image";
 
 //NavBar component
 type NavBarProps = { bootcamps: bootcamp[] };
 const NavBar = ({ bootcamps }: NavBarProps) => {
   return (
-    <div className="flex md:grid border-b h-[60px] grid-cols-[1fr_4fr_1fr]">
+    <motion.div
+      initial={{ y: -30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      className="flex md:grid border-b h-[60px] grid-cols-[1fr_4fr_1fr]"
+    >
       <div className="flex justify-center items-center">
         <CustomButton //todo
         >
@@ -27,7 +32,7 @@ const NavBar = ({ bootcamps }: NavBarProps) => {
       <div className="flex justify-center items-center md:hidden text-primary-base">
         <MenuIcon size={36} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
