da8512db567b8575d02a0ec0ca82610412b2ad3b
Mohmd <mim.mohrechi@gmail.com>
2024-06-03 18:53:04 +0330

    finish what we do

--- src/components/Landing/WhatWeDo/WhatWeDo.tsx
+++ src/components/Landing/WhatWeDo/WhatWeDo.tsx
@@ -1,28 +1,31 @@
 "use client";
 
+import CustomHeading from "@/components/Ui/CustomHeading";
 import WhatWeDoItem from "./WhatWeDoItem";
 
 //WhatWeDo component
 const WhatWeDo = () => {
   return (
     <div className="flex flex-col ">
-      <h2>ما در کلاسور چه‌کار می‌کنیم؟</h2>
-      <div className="grid grid-cols-2 md:flex flex-col md:flex-row gap-5 mt-10 md:mb-10 justify-between">
+      <CustomHeading>
+        <h2>ما در کلاسور چه‌کار می‌کنیم؟</h2>
+      </CustomHeading>
+      <div className="grid grid-cols-2 md:flex flex-col md:flex-row gap-2 md:gap-5 mt-10 md:mb-10 justify-between">
         <WhatWeDoItem
-          text="تدریس توسط اساتید مطرح"
-          image="/Landing/Companies/company1.webp"
+          text="آموزش استاندارد در قالب بوتکمپ و دوره های آموزشی"
+          image="/Landing/WhatWeDo/1.png"
         />
         <WhatWeDoItem
           text="تدریس توسط اساتید مطرح"
-          image="/Landing/Companies/company1.webp"
+          image="/Landing/WhatWeDo/2.png"
         />
         <WhatWeDoItem
-          text="تدریس توسط اساتید مطرح"
-          image="/Landing/Companies/company1.webp"
+          text="ارائه مدرک معتبر و قابل ارائه جهت استخدام"
+          image="/Landing/WhatWeDo/3.png"
         />
         <WhatWeDoItem
-          text="تدریس توسط اساتید مطرح"
-          image="/Landing/Companies/company1.webp"
+          text="معرفی نیروی مستعد به سازمان ها جهت استخدام پس از آموزش استاندارد"
+          image="/Landing/WhatWeDo/4.png"
         />
       </div>
     </div>
--- src/components/Landing/WhatWeDo/WhatWeDoItem.tsx
+++ src/components/Landing/WhatWeDo/WhatWeDoItem.tsx
@@ -10,16 +10,16 @@ type WhatWeDoItemProps = {
 const WhatWeDoItem = ({ text, image }: WhatWeDoItemProps) => {
   return (
     <div className="flex flex-col p-2 w-full bg-white aspect-[3/4] rounded-[20px]">
-      <div className="w-full h-2/3 bg-light-4 rounded-[12px] overflow-hidden">
+      <div className="w-full p-5 md:p-10 h-2/3 bg-light-4 rounded-[12px] overflow-hidden">
         <Image
           src={image}
-          height={270}
-          width={270}
+          height={512}
+          width={512}
           alt={text}
           className="size-full object-contain"
         />
       </div>
-      <span className="flex justify-center items-center font-bold grow text-gray-3">
+      <span className="flex text-sm md:text-base text-center justify-center items-center font-bold grow text-gray-3">
         {text}
       </span>
     </div>
