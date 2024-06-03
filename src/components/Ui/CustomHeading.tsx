a77bb6bbe6d305c5b69d16ca308b601efdc8888c
Mohmd <mim.mohrechi@gmail.com>
2024-06-03 21:38:04 +0330

    add side option to CustomHeading

--- src/components/Ui/CustomHeading.tsx
+++ src/components/Ui/CustomHeading.tsx
@@ -6,16 +6,20 @@ import { ReactNode } from "react";
 //CustomHeading component
 type CustomHeadingProps = {
   children: ReactNode;
+  side?: "left" | "right";
 };
-const CustomHeading = ({ children }: CustomHeadingProps) => {
+const CustomHeading = ({ children, side = "right" }: CustomHeadingProps) => {
   return (
-    <div className="w-fit">
+    <div
+      className={`flex flex-col w-fit ${side === "left" ? "items-end self-end" : null}`}
+    >
       {children}
-      <motion.div 
-      initial={{width:0}}
-      whileInView={{width:"50%"}}
-      transition={{delay:0.200}}
-      className="w-1/2 h-1 mt-2 bg-primary-20 rounded-full"/>
+      <motion.div
+        initial={{ width: 0 }}
+        whileInView={{ width: "50%" }}
+        transition={{ delay: 0.2 }}
+        className="h-1 mt-2 bg-primary-20 rounded-full"
+      />
     </div>
   );
 };
