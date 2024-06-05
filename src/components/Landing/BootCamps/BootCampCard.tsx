a0a6598eecf7212067b895b2f975d2a1f4eb8a73
Mohmd <mim.mohrechi@gmail.com>
2024-06-05 15:00:21 +0330

    add animation to bootcams of landing

--- src/components/Landing/BootCamps/BootCampCard.tsx
+++ src/components/Landing/BootCamps/BootCampCard.tsx
@@ -1,19 +1,27 @@
 "use client";
+import { motion } from "framer-motion";
 import { InfoIcon } from "lucide-react";
 //BootCampCard component
 const title = "پایتون / جنگو";
 const BootCampCard = () => {
+  const variants = {
+    initial: { opacity: 0, x: 100 },
+    animate: { opacity: 1, x: 0 },
+  };
   return (
-    <div className="flex flex-col bg-white p-2 rounded-[20px] aspect-square">
+    <motion.div
+      variants={variants}
+      className="flex flex-col bg-white p-2 rounded-[20px] aspect-square"
+    >
       <div className="bg-primary-base h-3/5 rounded-[12px]">todo</div>
       <div className="text-lg font-bold flex grow items-center justify-center">
         {title}
       </div>
       <button className="text-sm gap-2 grow font-thin text-gray-4 flex items-center justify-center border-t-light-3 border-t">
-        <InfoIcon size={14}/>
+        <InfoIcon size={14} />
         اطلاعات بیشتر
       </button>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Landing/BootCamps/BootCamps.tsx
+++ src/components/Landing/BootCamps/BootCamps.tsx
@@ -2,22 +2,40 @@
 
 import CustomHeading from "@/components/Ui/CustomHeading";
 import BootCampCard from "./BootCampCard";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 //BootCamps component
 const BootCamps = () => {
+  const triggerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(triggerRef, { once: true ,margin:"0px 0px -100px 0px" });
+  const Pvariant = {
+    animate: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.1, // Stagger animation by 0.1 seconds between children
+        when: "beforeChildren",
+      },
+    },
+  };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" ref={triggerRef}>
       <CustomHeading>
         <h2>رشته‌های کلاسور</h2>
       </CustomHeading>
-      <div className="grid gap-2 md:gap-5 w-full mt-10 grid-cols-2 md:grid-cols-4">
+      <motion.div 
+        variants={Pvariant}
+        initial="initial"
+        animate={isInView ? "animate" : "initial"}
+      className="grid gap-2 md:gap-5 w-full mt-10 grid-cols-2 md:grid-cols-4">
         {/*todo todo: map data from backend */}
         <BootCampCard />
         <BootCampCard />
         <BootCampCard />
         <BootCampCard />
         <BootCampCard />
-      </div>
+      </motion.div>
     </div>
   );
 };
