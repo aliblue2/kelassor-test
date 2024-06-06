31dee37dd5d55448cc47503dfce9a1484a594e13
Mohmd <mim.mohrechi@gmail.com>
2024-06-06 15:13:40 +0330

    add hover effect

--- src/components/Ui/CustomButton.tsx
+++ src/components/Ui/CustomButton.tsx
@@ -7,7 +7,7 @@ interface CustomButtonProps
 const CustomButton = ({ children, className, ...props }: CustomButtonProps) => {
   return (
     <button
-      className={`px-8 py-2 text-white bg-primary-base rounded-[10px] ${className}`}
+      className={`px-8 py-2 text-white hover:scale-105 duration-200 bg-primary-base rounded-[10px] ${className}`}
       {...props}
     >
       {children}
