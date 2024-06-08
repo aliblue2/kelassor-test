a940b91bc4b179128840c6a57732965710ec6af2
Mohmd <mim.mohrechi@gmail.com>
2024-06-08 22:39:51 +0330

    minor change to cusotm button for better phone ui

--- src/components/Ui/CustomButton.tsx
+++ src/components/Ui/CustomButton.tsx
@@ -7,7 +7,7 @@ interface CustomButtonProps
 const CustomButton = ({ children, className, ...props }: CustomButtonProps) => {
   return (
     <button
-      className={`px-8 py-2 text-white hover:scale-105 duration-200 bg-primary-base rounded-[10px] ${className}`}
+      className={` px-4 md:px-8 py-2 text-sm md:text-base text-white hover:scale-105 duration-200 bg-primary-base rounded-[10px] ${className}`}
       {...props}
     >
       {children}
