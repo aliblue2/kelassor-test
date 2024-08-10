fe2669f65566186be7a25aed6af17fde260a598c
Mohmd <mim.mohrechi@gmail.com>
2024-08-10 20:04:21 +0330

    fix

--- src/app/(home)/user-panel/bootcamp/page.tsx
+++ src/app/(home)/user-panel/bootcamp/page.tsx
@@ -1,16 +1,11 @@
 import { getPanelBootcamps } from "@/requests/user-panel/getPanelBootcamps";
-import { toJalaali } from "jalaali-js";
+import img from "/public/common/snappPay.svg";
 import { cookies } from "next/headers";
+import Image from "next/image";
 
 //page component
 const page = async () => {
   const res = await getPanelBootcamps(cookies().get("session_id")?.value);
-  console.log(23434, res);
-  const convertDate = (input: string) => {
-    const date = new Date(input);
-    const jalaali = toJalaali(date);
-    return jalaali.jy + "/" + jalaali.jm + "/" + jalaali.jd;
-  };
   return (
     <div className="flex flex-col gap-10 py-10 grow">
       <h3 className="self-start">بوتکمپ‌های من</h3>
@@ -19,21 +14,30 @@ const page = async () => {
           <div>نام بوتکمپ</div>
           <div>تاریخ شروع</div>
           <div>مدت دوره</div>
-          <div>پرداخت با اسنپ‌پی</div>
+          <div className="flex items-center justify-between gap-2">
+            پرداخت با 
+            <Image src={img} height={30} width={50} alt="snapp pay" className="brightness-[125%]" />
+          </div>
         </div>
         {/******************************************************************************
           data */}
         {res.map((item) => (
           <div
-            key={item.key}
+            key={item.transactionId}
             className="grid grid-cols-4 gap-2 justify-items-center items-center p-2 odd:bg-light-2 even:bg-light-3"
           >
             <div>{item.bootcampTitle}</div>
-            
+
             <div>{item.start}</div>
-              
+
             <div>{item.length}</div>
-            <a href={item.link} target="_blank">کلیک کنید</a>
+            <a
+              href={item.link}
+              className="flex gap-2 items-center justify-between"
+              target="_blank"
+            >
+              برای پرداخت کلیک کنید
+            </a>
           </div>
         ))}
       </div>
