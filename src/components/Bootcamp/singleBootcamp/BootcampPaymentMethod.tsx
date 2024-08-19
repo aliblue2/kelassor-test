e85ab50a12b505fe58646fa31399878925c4a86d
Mohmd <mim.mohrechi@gmail.com>
2024-08-19 20:56:01 +0330

    payment method change

--- src/components/Bootcamp/singleBootcamp/BootcampPaymentMethod.tsx
+++ src/components/Bootcamp/singleBootcamp/BootcampPaymentMethod.tsx
@@ -12,12 +12,17 @@ const BootcampPaymentMethod = ({ price }: { price: string }) => {
       </CustomHeading>
       <div className="flex flex-col md:flex-row gap-5 text-xl">
         <div className="flex flex-1 hover:shadow2 gap-2 border border-[#EBECF2] p-5 rounded-[20px] shadow2">
-          <div className=" font-bold bg-error flex flex-col items-center justify-center text-xl text-white size-24 rounded-[20px]">
-            <div>10%</div>
-            <div>تخفیف</div>
+          <div className=" font-bold bg-white p-2 flex flex-col items-center justify-center text-xl text-white size-24 rounded-[20px]">
+              <Image
+                src="/logo.png"
+                alt="لوگوی کلاسور"
+                height={96}
+                width={96}
+              />
           </div>
           <div className="flex flex-col items-start gap-1 justify-center">
             <div className="font-bold ">پرداخت نقدی</div>
+            <div >کارشناسان ما هماهنگ خواهند کرد</div>
           </div>
         </div>
         <div className="flex flex-1 hover:shadow2 gap-2 border border-[#EBECF2] p-5 rounded-[20px] shadow2">
