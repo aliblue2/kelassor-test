006ebe31778912577d2d8ad08b1a355654918ca3
Mohmd <mim.mohrechi@gmail.com>
2024-08-19 20:04:00 +0330

    payment-success: add tid

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -3,19 +3,32 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 
 //page component
-const page = async () => {
-  const res = await getPaymentStatus(cookies().get("session_id")?.value);
+const page = async ({
+  searchParams,
+}: {
+  searchParams: { transactionId: string | undefined };
+}) => {
+  let res;
+  if (!searchParams.transactionId) {
+    res = { successful: false };
+  } else {
+    res = await getPaymentStatus(
+      cookies().get("session_id")?.value,
+      searchParams.transactionId,
+    );
+  }
   return (
     <div className="flex gap-10 items-center flex-col grow justify-center">
-        {res.successful ? (
-          <>
-            <h1 className="text-primary-base">پرداخت با موفقیت انجام شد</h1>
-          </>
-        ) : (
-          <h1 className="text-error">مشکلی پیش آمد</h1>
-        )}
-        <Link href={"/user-panel"}>بازگشت به پنل کاربری </Link>
-      </div>
+      {res.successful ? (
+        <>
+          <h1 className="text-primary-base">پرداخت با موفقیت انجام شد</h1>
+          <div>شناسه تراکنش : {searchParams.transactionId}</div>
+        </>
+      ) : (
+        <h1 className="text-error">مشکلی پیش آمد</h1>
+      )}
+      <Link href={"/user-panel"}>بازگشت به پنل کاربری </Link>
+    </div>
   );
 };
 
