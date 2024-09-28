96175ad46e7ced1b14bb5812f3908c9af11adba6
alireza <alireza@git.com>
2024-09-28 17:06:55 +0330

    add yektanet analytics script to root layout - fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,20 +15,25 @@ export default async function RootLayout({
   return (
     <html dir="rtl" lang="en">
       <head>
-        <Script id="yektanet-analytics" strategy="afterInteractive">
-          {`
-            !function (t, e, n) {
-              t.yektanetAnalyticsObject = n, t[n] = t[n] function () {
-                t[n].q.push(arguments)
-              }, t[n].q = t[n].q [];
-              var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
-                  c = e.getElementsByTagName("script")[0], s = e.createElement("script");
-              s.id = "ua-script-Iyb2lWBT"; s.dataset.analyticsobject = n;
-              s.async = 1; s.type = "text/javascript";
-              s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/Iyb2lWBT/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
-            }(window, document, "yektanet");
-          `}
-        </Script>
+        <Script
+          id="yektanet-analytics"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (t, e, n) {
+                t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
+                  t[n].q.push(arguments)
+                }, t[n].q = t[n].q || [];
+                var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+                    c = e.getElementsByTagName("script")[0], s = e.createElement("script");
+                s.id = "ua-script-Iyb2lWBT"; s.dataset.analyticsobject = n;
+                s.async = 1; s.type = "text/javascript";
+                s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/Iyb2lWBT/rg.complete.js?v=" + r;
+                c.parentNode.insertBefore(s, c)
+              }(window, document, "yektanet");
+            `,
+          }}
+        />
       </head>
       <body className="flex overflow-y-auto overflow-x-hidden flex-col w-screen text-black min-h-dvh bg-background font-vazir">
         <Providers bootcamps={bootcamps}>{children}</Providers>
