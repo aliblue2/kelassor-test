43f320b2f91e6fc71c90d61fb2748c2963343756
alireza <alireza@git.com>
2024-11-25 13:33:28 +0330

    se commit test

--- src/app/admin/dashboard/page.js
+++
@@ -1,7 +0,0 @@
-import ADashboardPage from "@/components/admin/a-dashboard/ADashboardPage";
-
-const page = () => {
-  return <ADashboardPage />;
-};
-
-export default page;
--- src/app/admin/page.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-const page = () => {
-  redirect("/admin/dashboard");
-};
-
-export default page;
